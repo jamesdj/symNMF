4b1a28e5bc1692d37550d3f8033464d47836befa
James Jensen <james.jensen@recursionpharma.com>
2020-06-21 10:30:56 -0600

    profile and optimize; use EM for nans instead of masking

--- symNMF.py
+++ symNMF.py
@@ -1,13 +1,13 @@
 import os
-from functools import partial
 from itertools import cycle
 import warnings
+from functools import partial
 
 import numpy as np
+import scipy
 from sklearn.metrics import mean_squared_error
 from sklearn.utils import check_random_state
 from sklearn.exceptions import ConvergenceWarning
-import numpy.ma as ma
 from scipy.sparse.linalg import svds
 from joblib import Parallel, delayed
 
@@ -22,73 +22,6 @@ def shuffle_and_deal(cards, n_hands, random_state=None):
         hand = hands[next(cyc)]
         hand.append(card)
     return hands
-
-
-def emsvd(Y, k=None, tol=1E-3, max_iter=None):
-    """
-    From https://stackoverflow.com/questions/35577553/how-to-fill-nan-values-in-numeric-array-to-apply-svd
-
-    Approximate SVD on data with missing values via expectation-maximization
-
-    Inputs:
-    -----------
-    Y:          (nobs, ndim) data matrix, missing values denoted by NaN/Inf
-    k:          number of singular values/vectors to find (default: k=ndim)
-    tol:        convergence tolerance on change in trace norm
-    max_iter:    maximum number of EM steps to perform (default: no limit)
-
-    Returns:
-    -----------
-    Y_hat:      (nobs, ndim) reconstructed data matrix
-    mu_hat:     (ndim,) estimated column means for reconstructed data
-    U, s, Vt:   singular values and vectors (see np.linalg.svd and
-                scipy.sparse.linalg.svds for details)
-    """
-    if k is None:
-        svdmethod = partial(np.linalg.svd, full_matrices=False)
-    else:
-        svdmethod = partial(svds, k=k)
-    if max_iter is None:
-        max_iter = np.inf
-    # initialize the missing values to their respective column means
-    mu_hat = np.nanmean(Y, axis=0, keepdims=1)
-    valid = np.isfinite(Y)
-    Y_hat = np.where(valid, Y, mu_hat)
-    halt = False
-    ii = 1
-    v_prev = 0
-    while not halt:
-        # SVD on filled-in data
-        U, s, Vt = svdmethod(Y_hat - mu_hat)
-        # impute missing values
-        Y_hat[~valid] = (U.dot(np.diag(s)).dot(Vt) + mu_hat)[~valid]
-        # update bias parameter
-        mu_hat = Y_hat.mean(axis=0, keepdims=1)
-        # test convergence using relative change in trace norm
-        v = s.sum()
-        if ii >= max_iter or ((v - v_prev) / v_prev) < tol:
-            halt = True
-        ii += 1
-        v_prev = v
-
-    return Y_hat, mu_hat, U, s, Vt
-
-
-def ma_frob(X):
-    return ma.sqrt(ma.sum(X ** 2))
-
-
-def nmf_err(X, U, V):
-    return 0.5 * (ma_frob(ma.subtract(X, ma.dot(U, V.T))) ** 2) / (ma_frob(X) ** 2)
-
-
-def compute_default_lmda(x, U, V):
-    x_svd = emsvd(x)[0] if x.mask.sum() > 0 else x
-    svs = np.linalg.svd(x_svd, compute_uv=False)
-    max_sv = svs[0]
-    min_sv = svs[-1]
-    lmda = 1.01 * (0.5 * (max_sv + ma_frob(ma.subtract(x, ma.dot(U, V.T))) - min_sv))
-    return lmda
 
 
 def initialize_UV(X, r, random_state=None):
@@ -110,24 +43,38 @@ class SymNMF:
                  tol=1E-4,
                  alpha=0,
                  l1_ratio=0.5,
-                 random_state=None):
+                 random_state=None,
+                 warm_start_ab=False,
+                 warm_start_lmda=True,
+                 outer_max_iter=200,
+                 outer_tol=1E-2,
+                 ):
         self.n_components = n_components
         self.max_iter = max_iter
         self.tol = tol
         self.alpha = alpha
         self.l1_ratio = l1_ratio
         self.random_state = check_random_state(random_state)
+        self.warm_start_ab = warm_start_ab
+        self.warm_start_lmda = warm_start_lmda
+        self.outer_max_iter = outer_max_iter
+        self.outer_tol = outer_tol
         self.U = None
         self.V = None
 
     def fit(self, X, lmda=None):
-        U, V = symHALS(X, self.n_components,
-                       max_iter=self.max_iter,
-                       tol=self.tol,
-                       lmda=lmda,
-                       alpha=self.alpha,
-                       l1_ratio=self.l1_ratio,
-                       random_state=self.random_state)
+        func = partial(symHALSnan,
+                       warm_start_ab=self.warm_start_ab,
+                       warm_start_lmda=self.warm_start_lmda,
+                       outer_max_iter=self.outer_max_iter,
+                       outer_tol=self.outer_tol) if np.isnan(X).any() else symHALS
+        U, V = func(X, self.n_components,
+                    max_iter=self.max_iter,
+                    tol=self.tol,
+                    lmda=lmda,
+                    alpha=self.alpha,
+                    l1_ratio=self.l1_ratio,
+                    random_state=self.random_state)
         self.U = U
         self.V = V
         return self
@@ -143,46 +90,86 @@ def kth_diag_indices(a, k):
         return rows, cols
 
 
-def symHALS(Y_orig, J, max_iter=200, tol=1E-4, lmda=None, alpha=0, l1_ratio=0.5, random_state=None):
-    l1 = alpha * l1_ratio
-    l2 = alpha - l1
-    Y = ma.masked_invalid(Y_orig)
-    A, B = initialize_UV(Y, J, random_state=random_state)
-    if lmda is None:
-        lmda = compute_default_lmda(Y, A, B)
-    init_err = nmf_err(Y, A, B)
-    err = float(init_err)
-    n_iter = 0
-    n, m = Y.shape
-    n, J = A.shape
+def nmf_err(X, U, V):
+    UV = np.dot(U, V.T)
+    diff = np.subtract(X, UV)
+    diff_norm = np.linalg.norm(diff)
+    sq_diff_norm = diff_norm ** 2
+    err = 0.5 * sq_diff_norm
+    return err
+
+
+def compute_default_lmda(x, U, V):
+    # slower but maybe more stable?
+    # svs = np.linalg.svd(x, compute_uv=False, hermitian=True)
+    # max_sv = svs[0]
+    # min_sv = svs[-1]
+
+    # max_sv = scipy.sparse.linalg.svds(x, k=1, which='LM', return_singular_vectors=False)[0]
+    # min_sv = scipy.sparse.linalg.svds(x, k=1, which='SM', return_singular_vectors=False)[0]
+
+    # slightly faster, can do bc symmetric and so equivalent to singular vals
+    max_sv = scipy.sparse.linalg.eigsh(x, k=1, which='LM', return_eigenvectors=False)[0]
+    # trick to get small eigvals faster and with better convergence
+    # see https://docs.scipy.org/doc/scipy/reference/tutorial/arpack.html
+    min_sv = scipy.sparse.linalg.eigsh(x, k=1, sigma=0, which='LM', return_eigenvectors=False)[0]
+    lmda = 1.01 * (0.5 * (max_sv + np.linalg.norm(np.subtract(x, np.dot(U, V.T))) - min_sv))
+    return lmda
+
+
+def make_smoothing_matrix(m):
     S = np.zeros((m, m))
     S[kth_diag_indices(S, 1)] += 0.5
     S[kth_diag_indices(S, -1)] += 0.5
     S[0, 1] = 1
-    S[n - 1, n - 2] = 1
+    S[m - 1, m - 2] = 1  # would be n, but since symmetric
+    return S
+
+
+def symHALS(Y,
+            J,
+            A=None,
+            B=None,
+            max_iter=200,
+            tol=1E-4,
+            lmda=None,
+            alpha=0,
+            l1_ratio=0.5,
+            random_state=None):
+    n, m = Y.shape
+    assert n == m, f'Input matrix of dimension {Y.shape} is not square'
+    l1 = alpha * l1_ratio
+    l2 = alpha - l1
+    if A is None or B is None:
+        A, B = initialize_UV(Y, J, random_state=random_state)
+        init_err = nmf_err(Y, A, B)
+    else:
+        C, D = initialize_UV(Y, J, random_state=random_state)
+        init_err = nmf_err(Y, C, D)
+    if lmda is None:
+        lmda = compute_default_lmda(Y, A, B)
+    err = float(init_err)
+    S = make_smoothing_matrix(m)
     lmda_i = np.eye(n) * lmda
     Y_plus_lmda_i = Y + lmda_i
-    while n_iter < max_iter:
-        W = ma.dot(Y_plus_lmda_i.T, A)
-        V = ma.dot(A.T, A)
+
+    def hals_update(A, B):
+        W = np.dot(Y_plus_lmda_i.T, A)
+        V = np.dot(A.T, A)
         for j in range(J):
-            resid_term = B[:, j] * V[j, j] + W[:, j] - ma.dot(B, V[:, j])
+            resid_term = B[:, j] * V[j, j] + W[:, j] - np.dot(B, V[:, j])
             B[:, j] = (resid_term
-                       - lmda *
                        - l1 * np.ones(m)
-                       + l2 * ma.dot(S, B[:, j])).clip(0, np.inf) / (V[j, j] + l2 + lmda)
-        P = ma.dot(Y_plus_lmda_i, B)
-        Q = ma.dot(B.T, B)
-        for j in range(J):
-            resid_term = A[:, j] * Q[j, j] + P[:, j] - ma.dot(A, Q[:, j])
-            A[:, j] = (resid_term
-                       - l1 * np.ones(m)
-                       + l2 * ma.dot(S, A[:, j])).clip(0, np.inf) / (Q[j, j] + l2 + lmda)
+                       + l2 * np.dot(S, B[:, j])).clip(0, np.inf) / (V[j, j] + l2 + lmda)
+        return B
+
+    n_iter = 0
+    while n_iter < max_iter:
+        B = hals_update(A, B)
+        A = hals_update(B, A)
         new_err = nmf_err(Y, A, B)
-        #print(new_err, err)
-        #err_diff = err - new_err
-        #if err_diff < tol:
-        if 0 <= (err - new_err) / init_err < tol and n_iter > 10:
+        rel_err = (err - new_err) / init_err
+        if 0 <= rel_err < tol:
             break
         err = new_err
         n_iter += 1
@@ -190,6 +177,54 @@ def symHALS(Y_orig, J, max_iter=200, tol=1E-4, lmda=None, alpha=0, l1_ratio=0.5,
         warnings.warn("Maximum number of iterations %d reached. Increase it to"
                       " improve convergence." % max_iter, ConvergenceWarning)
     return A, B
+
+
+def symHALSnan(Y,
+               J,
+               warm_start_ab=False,
+               warm_start_lmda=True,
+               outer_max_iter=200,
+               outer_tol=1E-2,
+               random_state=None,
+               **kwargs):
+    """
+    Todo:
+    - explore convergence criteria more, make sure it's robust
+    - why are warm starts not faster?
+    - quality of solution and similarity of u and v with warm starts, not exact lambda
+      - it seems the lambda decreases as we go, so we are ok; the first lambda is larger than we need
+    """
+
+    random_state = check_random_state(random_state)
+    nanmask = np.isnan(Y)
+    yhat = Y.copy()
+    nanmean = np.nanmean(Y)
+    yhat[nanmask] = nanmean
+    first_vals = yhat[nanmask]
+    old_vals = first_vals.copy()
+    diff_ratio = 1
+    u, v = initialize_UV(yhat, J, random_state=random_state) if (warm_start_ab or warm_start_lmda) else (None, None)
+    lmda = compute_default_lmda(yhat, u, v) if warm_start_lmda else None
+    if not warm_start_ab:
+        u, v = None, None
+    n_iter = 0
+    kwargs.update({'lmda': lmda,
+                   'A': u,
+                   'B': v,
+                   'random_state': random_state})
+    while diff_ratio > outer_tol and n_iter < outer_max_iter:
+        u, v = symHALS(yhat,
+                       J,
+                       **kwargs)
+        y_nmf = np.dot(u, v.T)
+        new_vals = y_nmf[nanmask]
+        diff = np.mean((old_vals - new_vals) ** 2)
+        orig_diff = np.mean((first_vals - new_vals) ** 2)
+        diff_ratio = diff / orig_diff
+        yhat[nanmask] = new_vals
+        old_vals = new_vals
+        n_iter += 1
+    return u, v
 
 
 def add_reflected_indices(fold):
@@ -235,11 +270,11 @@ def symnmf_xval_rank(x, ncs, n_folds=10, n_reps=1, random_state=None, n_jobs=-1,
         reps_mses = []
         for r in range(n_reps):
             try:
+                nmf_kwargs.update({'n_components': nc})
                 mses = symnmf_xval(x,
                                    n_folds=n_folds,
                                    n_jobs=n_jobs,
                                    random_state=random_state,
-                                   n_components=nc,
                                    **nmf_kwargs)
             except Exception as e:
                 print(nc, e)
@@ -248,38 +283,4 @@ def symnmf_xval_rank(x, ncs, n_folds=10, n_reps=1, random_state=None, n_jobs=-1,
             reps_mses.extend(mses)
         msess.append(reps_mses)
     return msess
-
-
-
-"""
-# https://github.com/kimjingu/nonnegfac-python
-from nonnegfac.nnls import nnlsm_blockpivot
-
-def symANLS(X, r, max_iter=200, tol=1E-8, random_state=None):
-    # From Zhu et al. 2018 "Dropping Symmetry for Fast Symmetric Nonnegative Matrix Factorization"
-    
-    U, V = initialize_UV(X, r, random_state=random_state)
-    lmda = compute_default_lmda(X, U, V)
-    err = nmf_err(X, U, V)
-    n, r = V.shape
-    i = 0
-    while i <= max_iter:
-        UT, ut_info = nnlsm_blockpivot(np.concatenate([V, np.sqrt(lmda) * np.eye(r)]), 
-                              np.concatenate([X.T, np.sqrt(lmda) * V.T]), 
-                              False, 
-                              U.T)
-        U = UT.T
-        VT, vt_info = nnlsm_blockpivot(np.concatenate([U, np.sqrt(lmda) * np.eye(r)]),
-                              np.concatenate([X, np.sqrt(lmda) * U.T]), 
-                              False, 
-                              V.T)
-        V = VT.T
-        new_err = nmf_err(X, U, V)
-        err_diff = err - new_err
-        err = new_err
-        if err_diff < tol:
-            break
-        i += 1
-    return U, V
-"""
 
